b6928566eeaccb5bcc3d13ec842c7e219f2fd732
German704 <germaninsaurralde704@gmail.com>
2022-06-09 11:37:19 -0300

    agrego nuevos cambiaos en la clase

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, "public")))
 
 
-app.get("/", (req, res) => res.send("comision17"));
-app.get("/home", (req, res) => res.sendFile(path.join(__dirname, "views", "home.html")));
+// app.get("/", (req, res) => res.send("comision17"));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "home.html")));
 app.get("/contacto", (req, res) => res.sendFile(path.resolve(__dirname, "views", "contacto.html")));
+app.get("/login", (req, res) => res.sendFile(path.resolve(__dirname, "views", "login.html")));
+app.get("/carrito", (req, res) => res.sendFile(path.resolve(__dirname, "views", "carrito.html")));
+app.get("/register", (req, res) => res.sendFile(path.resolve(__dirname, "views", "register.html")));
+
+
 
 app.listen(port, () => console.log(`servidor levantado correctamente en http://localhost:${port}`));
 
